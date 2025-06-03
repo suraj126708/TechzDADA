1770df7d09e4bde5ee9b2a428fd89fa3f0a6ee7a
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2025-06-03 08:55:06 +0530

    production changes app

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./App.css";
 import UserRoute from "./UserRoute";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/next"; // Import Analytics
 
 function App() {
   return (
     <div className="App">
-      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<UserRoute />} />
         </Routes>
       </BrowserRouter>
       <ToastContainer />
+      <Analytics />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="603424006922-01kk45bggsuvdg5oaa0dfh492igpu3db.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
+    <Analytics />
+    <App />
   </React.StrictMode>
 );
