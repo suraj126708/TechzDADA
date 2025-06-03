a0ffb4ec8705f3db607452b5d38cb2d238075a53
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2025-06-03 08:50:47 +0530

    production commits

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./App.css";
 import UserRoute from "./UserRoute";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<UserRoute />} />
